1d143a65c229768ba77f43240d79a2f524fd927a
enamulhaque7 <enamulhaquemb01@gmail.com>
2021-06-23 15:39:46 +0600

    updating tabs

--- 06-tabs/src/App.js
+++ 06-tabs/src/App.js
@@ -24,11 +24,38 @@ function App() {
       <h1>Loading...</h1>
     </section>
   }
+  const { dates, company, duties, title } = jobs[value];
 
+  // console.log(value)
   return (
-    <main>
-      <h1>Hello</h1>
-    </main>
+    <section className="section">
+      <div className="title">
+        <h3>Experience</h3>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+        <div className="btn-container">
+          {
+            jobs.map((item, index) =>{
+              return <button className={`job-btn ${index === value && `active-btn`}`} key={item.id} onClick={() => setValue(index)}>{item.company}</button>;
+            })
+          }
+        </div>
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          {
+            duties.map((duty, index) => {
+              return <div key={index} className="job-desc">
+                <FaAngleDoubleRight className="job-icon" />
+                <p>{duty}</p>
+              </div>
+            })
+          }
+        </article>
+      </div>
+    </section>
   );
 }
 
